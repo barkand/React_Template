2ff2c17177a5febd3b245d18d01da3303d12b7ae
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-10 21:17:49 +0430

    🧑‍💻 Change Size of Title in Header

--- client/src/Template/Layout/Header/Toys/Title.js
+++ client/src/Template/Layout/Header/Toys/Title.js
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 export default function Title() {
   return (
     <>
-      <Typography variant="h6" component="div">
+      <Typography variant="subtitle1" component="div">
         {process.env.REACT_APP_COMPANY_NAME}
       </Typography>
     </>
