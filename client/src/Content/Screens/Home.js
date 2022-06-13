c1e3df6ad30d3f34a559a25cf8aa3bafd2996aef
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-13 23:27:43 +0430

    ✨ Add Title to Screens

--- client/src/Content/Screens/Home.js
+++ client/src/Content/Screens/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Home() {
+  document.title = process.env.REACT_APP_COMPANY_NAME;
+
   return (
     <>
       <div>Home</div>
--- client/src/Content/Screens/Page1.js
+++ client/src/Content/Screens/Page1.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Page1() {
+  document.title = process.env.REACT_APP_COMPANY_NAME + " | Page1";
+
   return (
     <>
       <div>Page 1</div>
