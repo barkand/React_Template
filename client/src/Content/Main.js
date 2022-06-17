57224c943874459e905dcd07bd293a528317c58d
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-17 15:17:20 +0430

    ✨ Add Code Splitting

--- client/src/Content/Main.js
+++ client/src/Content/Main.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./Screens/Home";
-import Page1 from "./Screens/Page1";
-import AlertBar from "../Template/Layout/Toys/AlertBar";
-
 export default function Main() {
+  const Home = React.lazy(() => import("./Screens/Home"));
+  const Page1 = React.lazy(() => import("./Screens/Page1"));
+
   return (
-    <main style={{ textAlign: "center", minHeight: "103vh" }}>
+    <React.Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/Page1" element={<Page1 />} />
       </Routes>
-      <AlertBar />
-    </main>
+    </React.Suspense>
   );
 }
--- client/src/Template/Layout/Layout.js
+++ client/src/Template/Layout/Layout.js
@@ -4,13 +4,17 @@ import Header from "./Header/Header";
 import Main from "../../Content/Main";
 import Footer from "./Footer/Footer";
 import ScrollTopButton from "./Toys/ScrollTop";
+import AlertBar from "./Toys/AlertBar";
 
 export default function Layout() {
   return (
     <>
       <Header />
       <ScrollTopButton>
-        <Main />
+        <main style={{ textAlign: "center", minHeight: "103vh" }}>
+          <Main />
+          <AlertBar />
+        </main>
       </ScrollTopButton>
       <Footer />
     </>
