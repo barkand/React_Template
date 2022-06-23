53981c6d648e1032d4d721746bd579b248d4b6ea
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-24 03:40:53 +0430

    ♻️ Change Mongo API for Save & Get Auth Code

--- server/database/mongo/auth.js
+++ server/database/mongo/auth.js
@@ -1,17 +1,40 @@
 const UserModel = require("./models/user");
+const { GetActiveCode } = require("../../auth/AuthCode");
 
-let SaveUser = (username, token, refresh_token) => {
-  UserModel.exists({ username: username }, function (err, doc) {
-    if (err) {
-      console.log(err);
-    } else if (!doc) {
-      UserModel.create({
-        username: username,
-        token: token,
-        refresh_token: refresh_token,
-      });
-    }
-  });
+let SeveAuthCode = async (username) => {
+  let user = await UserModel.findOne({ username: username });
+  if (!user) {
+    user = new UserModel({
+      username: username,
+      code: "",
+      code_expire: "",
+    });
+  }
+  let { active_code, expire_code } = GetActiveCode();
+  user.code = active_code;
+  user.code_expire = expire_code;
+  await user.save();
+
+  return active_code;
 };
 
-module.exports = { SaveUser };
+let GetAuthCode = async (username) => {
+  let user = await UserModel.findOne({ username: username });
+  if (!user) {
+    return 0;
+  }
+  if (user.code_expire < new Date()) {
+    return 0;
+  }
+  return user.code;
+};
+
+let SaveUser = async (username, token, refresh_token) => {
+  let user = await UserModel.findOne({ username: username });
+  if (user) {
+    user.token = token;
+    user.refresh_token = refresh_token;
+  }
+};
+
+module.exports = { SeveAuthCode, GetAuthCode, SaveUser };
