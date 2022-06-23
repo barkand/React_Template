922b811ab8b8354c7110c3a0bd16ebd65a4e6b29
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-24 03:23:06 +0430

    ♻️ Remove try..catch

--- server/database/maria/auth.js
+++ server/database/maria/auth.js
@@ -1,5 +1,5 @@
 const mariadb = require("mariadb");
-const { GetActiveCode, GetExpireCode } = require("../../auth/AuthCode");
+const { GetActiveCode } = require("../../auth/AuthCode");
 
 let GetConnection = () => {
   return {
@@ -15,61 +15,55 @@ let SeveAuthCode = async (username) => {
   let conn = await mariadb.createConnection(GetConnection());
   let { active_code, expire_code } = GetActiveCode();
 
-  try {
-    const rows = await conn.query("SELECT 1 FROM `user` WHERE username = ?", [
-      username,
-    ]);
-    if (rows && rows.length == 0) {
-      await conn.query(
-        "INSERT INTO `user`(username, code, code_expire) value (?, ?, ?)",
-        [username, active_code, expire_code]
-      );
-    } else {
-      await conn.query(
-        `UPDATE user SET code = ?, code_expire = ? WHERE username = ?`,
-        [active_code, expire_code, username]
-      );
-    }
-    return active_code;
-  } finally {
-    conn.end();
+  const rows = await conn.query("SELECT 1 FROM `user` WHERE username = ?", [
+    username,
+  ]);
+
+  if (rows && rows.length == 0) {
+    await conn.query(
+      "INSERT INTO `user`(username, code, code_expire) value (?, ?, ?)",
+      [username, active_code, expire_code]
+    );
+  } else {
+    await conn.query(
+      `UPDATE user SET code = ?, code_expire = ? WHERE username = ?`,
+      [active_code, expire_code, username]
+    );
   }
+  conn.end();
+
+  return active_code;
 };
 
 let GetAuthCode = async (username) => {
   let conn = await mariadb.createConnection(GetConnection());
 
-  try {
-    const rows = await conn.query(
-      "SELECT code FROM `user` WHERE username = ? and code_expire > now()",
-      [username]
-    );
-    if (rows && rows.length == 0) {
-      return 0;
-    } else {
-      return rows[0].code;
-    }
-  } finally {
-    conn.end();
+  const rows = await conn.query(
+    "SELECT code FROM `user` WHERE username = ? and code_expire > now()",
+    [username]
+  );
+  conn.end();
+
+  if (rows && rows.length == 0) {
+    return 0;
+  } else {
+    return rows[0].code;
   }
 };
 
 let SaveUser = async (username, token, refresh_token) => {
   const conn = await mariadb.createConnection(GetConnection());
 
-  try {
-    const rows = await conn.query("SELECT 1 FROM `user` WHERE username = ?", [
-      username,
-    ]);
-    if (rows && rows.length == 0) {
-      await conn.query(
-        `UPDATE user SET token = ?, refresh_token = ? WHERE username = ?`,
-        [token, refresh_token, username]
-      );
-    }
-  } finally {
-    conn.end();
+  const rows = await conn.query("SELECT 1 FROM `user` WHERE username = ?", [
+    username,
+  ]);
+  if (rows && rows.length > 0) {
+    await conn.query(
+      `UPDATE user SET token = ?, refresh_token = ? WHERE username = ?`,
+      [token, refresh_token, username]
+    );
   }
+  conn.end();
 };
 
 module.exports = { SeveAuthCode, GetAuthCode, SaveUser };
