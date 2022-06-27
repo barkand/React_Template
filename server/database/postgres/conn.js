41ce73c8eff24cb847436440c4903c0b425d3636
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-06-28 00:25:38 +0430

    🧑‍💻 Add Const to db

--- server/database/postgres/conn.js
+++ server/database/postgres/conn.js
@@ -3,6 +3,6 @@ const pgp = require("pg-promise")({
 });
 
 const cn = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-db = pgp(cn);
+const db = pgp(cn);
 
 module.exports = db;
