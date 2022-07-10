b1ada7f1b19ef2f7890505ddf29fb7c9a54660b0
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-07-11 00:05:10 +0430

    ✅ Add describe to Tests

--- client/src/Template/Components/Auth/Web1/api/__tests__/Api.test.js
+++ client/src/Template/Components/Auth/Web1/api/__tests__/Api.test.js
@@ -4,56 +4,58 @@ const _mobile = "(+98) 912-844-1234";
 const _mail = "a@a.com";
 const _receivedCode = "1-2-3-4";
 
-it("sendMobile", async () => {
-  const result = await PostApi("sendMobile", {
-    username: _mobile,
+describe("Auth API", () => {
+  it("sendMobile", async () => {
+    const result = await PostApi("sendMobile", {
+      username: _mobile,
+    });
+    expect(result.status).toBe(200);
+    expect(result.data).toEqual({
+      status: 200,
+      message: "mobile success sended",
+    });
   });
-  expect(result.status).toBe(200);
-  expect(result.data).toEqual({
-    status: 200,
-    message: "mobile success sended",
-  });
-});
 
-it("sendMobileCode", async () => {
-  const result = await PostApi("sendMobileCode", {
-    username: _mobile,
-    receivedCode: _receivedCode,
+  it("sendMobileCode", async () => {
+    const result = await PostApi("sendMobileCode", {
+      username: _mobile,
+      receivedCode: _receivedCode,
+    });
+    expect(result.status).toBe(200);
+    expect(result.data).toEqual({
+      status: 200,
+      message: "code is true",
+    });
   });
-  expect(result.status).toBe(200);
-  expect(result.data).toEqual({
-    status: 200,
-    message: "code is true",
-  });
-});
 
-it("sendMail", async () => {
-  const result = await PostApi("sendMail", { username: _mail });
-  expect(result.status).toBe(200);
-  expect(result.data).toEqual({
-    status: 200,
-    message: "email success sended",
+  it("sendMail", async () => {
+    const result = await PostApi("sendMail", { username: _mail });
+    expect(result.status).toBe(200);
+    expect(result.data).toEqual({
+      status: 200,
+      message: "email success sended",
+    });
   });
-});
 
-it("sendMailCode", async () => {
-  const result = await PostApi("sendMailCode", {
-    username: _mail,
-    receivedCode: _receivedCode,
+  it("sendMailCode", async () => {
+    const result = await PostApi("sendMailCode", {
+      username: _mail,
+      receivedCode: _receivedCode,
+    });
+    expect(result.status).toBe(200);
+    expect(result.data).toEqual({
+      status: 200,
+      message: "code is true",
+    });
   });
-  expect(result.status).toBe(200);
-  expect(result.data).toEqual({
-    status: 200,
-    message: "code is true",
-  });
-});
 
-it("LoginAccount", async () => {
-  const result = await PostApi("login", { username: _mobile });
-  expect(result.data).toEqual({
-    connected: true,
-    user: _mobile,
-    token: expect.any(String),
-    refreshToken: expect.any(String),
+  it("LoginAccount", async () => {
+    const result = await PostApi("login", { username: _mobile });
+    expect(result.data).toEqual({
+      connected: true,
+      user: _mobile,
+      token: expect.any(String),
+      refreshToken: expect.any(String),
+    });
   });
 });
--- client/src/Template/Components/Auth/Web1/api/__tests__/Auth.test.js
+++ client/src/Template/Components/Auth/Web1/api/__tests__/Auth.test.js
@@ -5,17 +5,19 @@ const _mail = "a@a.com";
 const _receivedCode = "1-2-3-4";
 let _username = process.env.REACT_APP_AUTH_TYPE === "MOBILE" ? _mobile : _mail;
 
-it("SendUserName", async () => {
-  const result = await SendUserName(_username);
-  expect(result.status).toBe("success");
-});
+describe("Auth", () => {
+  it("SendUserName", async () => {
+    const result = await SendUserName(_username);
+    expect(result.status).toBe("success");
+  });
 
-it("SendCode", async () => {
-  const result = await SendCode(_username, _receivedCode);
-  expect(result.status).toBe("success");
-});
+  it("SendCode", async () => {
+    const result = await SendCode(_username, _receivedCode);
+    expect(result.status).toBe("success");
+  });
 
-it("LoginAccount", async () => {
-  const result = await LoginAccount(_username);
-  expect(result.auth && result.auth.connected).toBe(true);
+  it("LoginAccount", async () => {
+    const result = await LoginAccount(_username);
+    expect(result.auth && result.auth.connected).toBe(true);
+  });
 });
