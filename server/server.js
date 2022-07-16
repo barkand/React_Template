fb4f27f38dbc90e3276a499a94081886f937bfb7
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-07-17 01:18:17 +0430

    🐛 Move 404 error Middleware after routing

--- server/server.js
+++ server/server.js
@@ -10,14 +10,6 @@ app.use(cors());
 app.use(helmet()); // protect from vulnerabilities
 app.disable("x-powered-by"); // an extra layer of obscurity to reduce server fingerprinting
 
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!") // custom 404
-})
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!') // custom error handler
-})
-
 app.get("/", (req, res) => {
   res.send("/api");
 });
@@ -42,6 +34,14 @@ if (process.env.API_TYPE === "REST") {
     })
   );
 }
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!"); // custom 404
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!"); // custom error handler
+});
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`🚀  Server is running on port ${process.env.SERVER_PORT}`);
