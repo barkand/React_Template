64077768b9987b0962d9b6148602e8b1937987e4
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-07 18:27:29 +0430

    Add Link to Logo of Header

--- client/src/Template/Layout/Header/Toys/Logo.js
+++ client/src/Template/Layout/Header/Toys/Logo.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../../../Content/Media/Logo.png";
 
 export default function Logo() {
   return (
-    <>
+    <Link to="/">
       <img
         alt="Logo"
         src={logo}
         style={{ width: "30px", marginRight: "20px" }}
       />
-    </>
+    </Link>
   );
 }
--- client/src/Template/Layout/Header/Toys/Title.js
+++ client/src/Template/Layout/Header/Toys/Title.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Typography } from "@mui/material";
 
 export default function Title() {
   return (
-    <>
+    <Link to="/">
       <Typography variant="subtitle1" component="div">
         {process.env.REACT_APP_COMPANY_NAME}
       </Typography>
-    </>
+    </Link>
   );
 }
