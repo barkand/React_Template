24be1689ba51833930bbc9753447de97bd80e435
barkand <barkand@users.noreply.github.com>
2022-08-07 14:00:20 +0000

    deploy: 64077768b9987b0962d9b6148602e8b1937987e4

--- static/js/5.dc5854e2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[5],{1024:function(t,c,e){"use strict";e.r(c),e.d(c,"default",(function(){return o}));e(1);var n=e(163),i=e(0);function o(){return Object(i.jsxs)("center",{children:[Object(i.jsx)(n.b,{to:"/",children:"Go to Home "}),Object(i.jsx)("p",{style:{fontSize:"20vw",color:"#cacaca"},children:"404"})]})}}}]);
-//# sourceMappingURL=5.dc5854e2.chunk.js.map
