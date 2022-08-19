03521a58ce2713edb39b07224dfe9a6f4095a397
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-08-19 21:23:05 +0430

    Add Prometheus Metrics

--- server/server.js
+++ server/server.js
@@ -1,14 +1,48 @@
-const app = require("express")();
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 require("dotenv").config();
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet()); // protect from vulnerabilities
 app.disable("x-powered-by"); // an extra layer of obscurity to reduce server fingerprinting
+
+// Promatheus Metrics
+if (process.env.PROMATHEUS === "true") {
+  const pino = require("pino");
+  const expressPino = require("express-pino-logger");
+  const promClient = require("prom-client");
+
+  const logger = pino({ level: process.env.LOG_LEVEL });
+  const expressLogger = expressPino({ logger });
+  app.use(express.json(), expressLogger);
+
+  const register = new promClient.Registry();
+  promClient.collectDefaultMetrics({ register });
+
+  // Create a counter metric
+  const counter = new promClient.Counter({
+    name: "http_requests_total",
+    help: "http requests total",
+    labelNames: ["handler", "method", "statuscode"],
+  });
+  register.registerMetric(counter);
+
+  // Setup server to Prometheus scrapes:
+  app.get("/metrics", async (req, res) => {
+    try {
+      res.set("Content-Type", register.contentType);
+      res.end(await register.metrics());
+    } catch (ex) {
+      res.status(500).end(ex);
+    }
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("/api");
