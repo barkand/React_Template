51e13aacbc2c64971634f48c1c89cf336852da42
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-09-05 01:06:10 +0430

    Add describe for better result

--- server/api/rest/v1/test/routers.test.js
+++ server/api/rest/v1/test/routers.test.js
@@ -1,5 +1,6 @@
 // Custom Config
 process.env.API_TYPE = "REST";
+const API_PATH = "/api/rest/v1";
 
 let chai = require("chai");
 let chaiHttp = require("chai-http");
@@ -8,120 +9,126 @@ let server = require("../../../../server");
 
 chai.use(chaiHttp);
 
-// Check Mobile Apis
-describe("===== Mobile =====", () => {
-  describe("/sendMobile", () => {
-    it("it should POST a Mobile number", (done) => {
-      let data = {
-        username: "1234",
-      };
+describe("========== REST ==========", () => {
+  // Check Mobile Apis
+  describe("===== Mobile =====", () => {
+    describe("/sendMobile", () => {
+      it("it should POST a Mobile number", (done) => {
+        let data = {
+          username: "1234",
+        };
 
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMobile")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property("message").eql("mobile success sended");
-          done();
-        });
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMobile")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have
+              .property("message")
+              .eql("mobile success sended");
+            done();
+          });
+      });
+    });
+
+    describe("/sendMobileCode --Invalid Code", () => {
+      it("it should POST a received code and get error", (done) => {
+        let data = {
+          username: "1234",
+          receivedCode: "1-2-3-4",
+        };
+
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMobileCode")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.have.property("message").eql("code is wrong");
+            done();
+          });
+      });
+    });
+
+    describe("/sendMobileCode --Empty Code", () => {
+      it("it should POST a empty code and get error", (done) => {
+        let data = {
+          username: "1234",
+          receivedCode: "",
+        };
+
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMobileCode")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.have.property("message").eql("code is wrong");
+            done();
+          });
+      });
     });
   });
 
-  describe("/sendMobileCode --Invalid Code", () => {
-    it("it should POST a received code and get error", (done) => {
-      let data = {
-        username: "1234",
-        receivedCode: "1-2-3-4",
-      };
+  // Check Mail Apis
+  describe("===== Mail =====", () => {
+    describe("/sendMail", () => {
+      it("it should POST a Mail number", (done) => {
+        let data = {
+          username: "a@a.a",
+        };
 
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMobileCode")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property("message").eql("code is wrong");
-          done();
-        });
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMail")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have
+              .property("message")
+              .eql("email success sended");
+            done();
+          });
+      });
     });
-  });
 
-  describe("/sendMobileCode --Empty Code", () => {
-    it("it should POST a received code and get error", (done) => {
-      let data = {
-        username: "1234",
-        receivedCode: "",
-      };
+    describe("/sendMailCode --Invalid Code", () => {
+      it("it should POST a received code and get error", (done) => {
+        let data = {
+          username: "a@a.a",
+          receivedCode: "1-2-3-4",
+        };
 
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMobileCode")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property("message").eql("code is wrong");
-          done();
-        });
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMailCode")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.have.property("message").eql("code is wrong");
+            done();
+          });
+      });
     });
-  });
-});
 
-// Check Mail Apis
-describe("===== Mail =====", () => {
-  describe("/sendMail", () => {
-    it("it should POST a Mail number", (done) => {
-      let data = {
-        username: "a@a.a",
-      };
+    describe("/sendMailCode --Empty Code", () => {
+      it("it should POST a empty code and get error", (done) => {
+        let data = {
+          username: "a@a.a",
+          receivedCode: "",
+        };
 
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMail")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property("message").eql("email success sended");
-          done();
-        });
-    });
-  });
-
-  describe("/sendMailCode --Invalid Code", () => {
-    it("it should POST a received code and get error", (done) => {
-      let data = {
-        username: "a@a.a",
-        receivedCode: "1-2-3-4",
-      };
-
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMailCode")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property("message").eql("code is wrong");
-          done();
-        });
-    });
-  });
-
-  describe("/sendMailCode --Empty Code", () => {
-    it("it should POST a received code and get error", (done) => {
-      let data = {
-        username: "a@a.a",
-        receivedCode: "",
-      };
-
-      chai
-        .request(server)
-        .post("/api/rest/v1/sendMailCode")
-        .send(data)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.have.property("message").eql("code is wrong");
-          done();
-        });
+        chai
+          .request(server)
+          .post(API_PATH + "/sendMailCode")
+          .send(data)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.have.property("message").eql("code is wrong");
+            done();
+          });
+      });
     });
   });
 });
